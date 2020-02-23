4c724749414b0db67948c1ba484315ffa3f0bb36
ahallam <alexhallam75@gmail.com>
2020-02-23 13:28:50 -0500

    Navigation with React Router implemented

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,38 @@
 import React from "react";
+import { ThemeProvider } from "@material-ui/core/styles";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Theme from "./ui/Theme";
 import Header from "../components/ui/Header";
-import { ThemeProvider } from "@material-ui/core/styles";
 
 function App() {
   return (
     <ThemeProvider theme={Theme}>
-      <Header />
-      Hello!!!
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={() => <div>Home</div>} />
+          <Route exact path="/services" component={() => <div>Sevices</div>} />
+          <Route
+            exact
+            path="/customsoftware"
+            component={() => <div>Custom Software</div>}
+          />
+          <Route
+            exact
+            path="/mobileapps"
+            component={() => <div>Mobile Apps</div>}
+          />
+          <Route exact path="/websites" component={() => <div>websites</div>} />
+          <Route
+            exact
+            path="/revolution"
+            component={() => <div>Revolution</div>}
+          />
+          <Route exact path="/about" component={() => <div>About</div>} />
+          <Route exact path="/contact" component={() => <div>Contact</div>} />
+          <Route exact path="/estimate" component={() => <div>Estimate</div>} />
+        </Switch>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
